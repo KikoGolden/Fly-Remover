b8326a62ebb452132a647623a421cdcfb416bf90
Kiko Golden <94949765+KikoGolden@users.noreply.github.com>
2022-07-27 16:13:08 +0300

    Add files via upload

--- fly-remover/script.js
+++ fly-remover/script.js
@@ -8,13 +8,35 @@ root.addEventListener('click', () => {
     }, 200)
 })
 
-let flies = document.querySelectorAll('.fly').forEach(x => x.addEventListener('click', () =>{
-    if (x.src != './images/blood-splash.png') {
-        x.src= './images/blood-splash.png';
-        kills++;
-        setTimeout(() =>{
-            x.style.display="none";
-        }, 700)
-    }
-   
-}))
+
+//fly spawn
+setInterval(() =>{
+    const height = randomIntFromInterval(10, 90)// y
+    const width = randomIntFromInterval(10, 90)// x
+    let fly = document.createElement('img');
+
+    fly.classList.add('fly');
+    fly.src= 'images/fly.png';
+    fly.style.left = width + 'vw';
+    fly.style.top = height + 'vh';
+    root.appendChild(fly);
+
+    document.querySelectorAll('.fly').forEach(x => x.addEventListener('click', () =>{
+        let path = x.src;
+        if (!path.includes('blood')) {
+            x.src= './images/blood-splash.png';
+            kills++;
+            console.log('kill');
+            setTimeout(() =>{
+                x.style.display="none";
+            }, 1000)
+        }
+       
+    }))
+}, 2000)
+
+
+//random number generator
+function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
